275c9887c3e18c617e9b4cc222a46aecfadde3f5
arm4rg <larmandoreyes502@gmail.com>
2026-08-04 23:31:20 -0600

    Agregar metodos ingresar numeros, sumar, restar y ciclo

--- src/ejercicio12/MenuBasico.java
+++ src/ejercicio12/MenuBasico.java
@@ -3,6 +3,30 @@ package ejercicio12;
 import java.util.Scanner;
 
 public class MenuBasico {
+  static float num1, num2;
+  static Scanner scanner = new Scanner(System.in);
+
+  public static void ingresarNumeros(){
+    System.out.print("Ingrese numero 1: ");
+    num1 = scanner.nextFloat();
+    System.out.print("Ingrese numero 2: ");
+    num2 = scanner.nextFloat();
+  }
+
+  public static void sumar(){
+    ingresarNumeros();
+    System.out.println("    ***Sumar 2 numeros***");
+    float suma = num1 + num2;
+    System.out.println("El resultado de " +num1 +"+" +num2 +" es " +suma);
+  }
+
+  public static void restar(){
+    ingresarNumeros();
+    System.out.println("    ***Restar 2 numeros***");
+    float resta = num1 - num2;
+    System.out.println("El resultado de " +num1 +"-" +num2 +" es " +resta);
+  }
+
   public static void menuBasico(){
     int opcion;
     Scanner scanner = new Scanner(System.in);
@@ -10,14 +34,16 @@ public class MenuBasico {
       System.out.println("    Menu");
       System.out.println("1.Sumar");
       System.out.println("2.Restar");
+      System.out.println("3.Salir");
+      System.out.print("Ingrese una opcion: ");
       opcion=scanner.nextInt();
         switch (opcion) {
           case 1:
-            System.out.println("suma");
+            sumar();
             break;
 
           case 2:
-            System.out.println("resta");
+            restar();
             break;
 
           case 3:
@@ -35,5 +61,6 @@ public class MenuBasico {
 
   public static void main(String[] args) {
     menuBasico();
+    scanner.close();
   }
 }
