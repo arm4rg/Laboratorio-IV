450d5a4e56ae707eda0d364e207a0742da3a4551
arm4rg <larmandoreyes502@gmail.com>
2026-08-04 21:55:00 -0600

    Agregar ejercicio 10; Estadisticas Basicas

---
+++ src/ejercicio10/EstadisticasBasicas.java
@@ -0,0 +1,49 @@
+package ejercicio10;
+
+import java.util.Scanner;
+
+public class EstadisticasBasicas {
+  static float[] numeros = new float[5];
+  static float suma;
+
+  
+  public static void ingresarNumeros(){
+    Scanner scanner = new Scanner(System.in);
+    for(int i=0; i<numeros.length; i++){
+      System.out.print("Ingrese numero " +(i+1) +": ");
+      numeros[i]= scanner.nextFloat();
+    }
+    suma();
+    scanner.close();
+  }
+  public static void suma(){
+    for(int i=0; i<numeros.length;i++){
+      suma += numeros[i];
+    }
+    System.out.println("La suma es " +suma);
+    promedio();
+  }
+
+  public static void promedio(){
+    float promedio=suma/numeros.length;
+    System.out.println("El promedio es " +promedio);
+    mayor();
+  }
+
+  public static void mayor(){
+    float mayor=numeros[0];
+      for (float f : numeros) {
+        if (f>mayor) {
+          mayor = f;
+        }
+      }
+      System.out.println("El numero mayor de los 5 numeros es: " +mayor);
+  }
+
+  public static void Menor(){
+    
+  }  
+  public static void main(String[] args) {
+    ingresarNumeros();
+  }
+}
